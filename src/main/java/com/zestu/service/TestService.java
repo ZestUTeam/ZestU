c87e8ccd02abf57e2f5e87c41c8269ad6c4a538a
yangc <yangc@DESKTOP-QUD3O48>
2017-06-06 17:12:48 +0800

    新建了登录注册action，Struts-yc.xml文件

--- src/main/java/com/zestu/service/TestService.java
+++ src/main/java/com/zestu/service/TestService.java
@@ -2,7 +2,6 @@ package com.zestu.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.cache.annotation.Caching;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
